0075af0da4ae8d76dede5683947216c246a2d760
Sinega Rajendran <sinega.rajendran1905@gmail.com>
2022-05-13 13:42:13 +0800

    First commit
    Web tour

---
+++ data.js
@@ -0,0 +1,170 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-erc_level-1_starbucks",
+      "name": "ERC_Level 1_Starbucks",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.3806487024864271,
+        "pitch": -0.06095719498370755,
+        "fov": 1.3793455132008117
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5147137478323742,
+          "pitch": 0.07677901367872586,
+          "rotation": 11.780972450961727,
+          "target": "1-erc_level-1_nearescalator_photo_1512"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.7497812250355462,
+          "pitch": -0.10350345666831906,
+          "title": "UTown Starbucks",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.7271062510596966,
+          "pitch": -0.05129564827344879,
+          "title": "Study space",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-erc_level-1_nearescalator_photo_1512",
+      "name": "ERC_Level 1_NearEscalator_PHOTO_1512",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.9972067017877286,
+        "pitch": 0.02131724488432063,
+        "fov": 1.3793455132008117
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6602545783172644,
+          "pitch": 0.054850672226340436,
+          "rotation": 0.7853981633974483,
+          "target": "0-erc_level-1_starbucks"
+        },
+        {
+          "yaw": -1.913721189226722,
+          "pitch": 0.1075063725020069,
+          "rotation": 5.497787143782138,
+          "target": "2-erc_level-1_tembusu-tree"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.2076351083796908,
+          "pitch": -0.07409580772011104,
+          "title": "Elevator leading to the 2nd Level",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.07515457742284148,
+          "pitch": -0.10204851400730419,
+          "title": "Stephen Riady Centre",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-erc_level-1_tembusu-tree",
+      "name": "ERC_Level 1_Tembusu Tree",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.617989352720933,
+        "pitch": -0.0757050661276395,
+        "fov": 1.3793455132008117
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4716399572062944,
+          "pitch": 0.1509150517951987,
+          "rotation": 0.7853981633974483,
+          "target": "1-erc_level-1_nearescalator_photo_1512"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.79499001759112,
+          "pitch": 0.1458067770913658,
+          "title": "Tembusu Tree",
+          "text": "A native tree to Singapore and its illustration can be found on $5 Singaporean note.&nbsp;"
+        },
+        {
+          "yaw": 2.1207457705655575,
+          "pitch": 0.1598482966887378,
+          "title": "Sets of benches&nbsp;",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "UTown Web Tour",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
